b74adff9b9f90c7c93e95608fdbfb024f1b4520e
Jakub <jakub.wojtowicz@sephcode.pl>
2024-03-06 09:41:21 +0100

    Initial commit
    Signed-off-by: Jakub <jakub.wojtowicz@sephcode.pl>

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+output "name" {
+  value = random_password.password.result
+  sensitive = true
+}
