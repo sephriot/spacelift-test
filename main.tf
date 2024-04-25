24f0c2eb2d18ca92cb6ac7bdea1ae0d4070cfcc2
Jakub <jakub.wojtowicz@sephcode.pl>
2024-04-25 13:25:48 +0200

    Added tests
    Signed-off-by: Jakub <jakub.wojtowicz@sephcode.pl>

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.0"
+    }
+  }
+}
+
 resource "random_password" "password" {
   length           = 16
   special          = true
